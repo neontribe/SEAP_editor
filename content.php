793f51f25a59668979f204ed41a787b3c0fe5d1b
katjam <katja@neontribe.co.uk>
2015-04-29 15:41:10 +0100

    A bit better error checking.

--- content.php
+++ content.php
@@ -1,9 +1,26 @@
 <h1>ESA Content</h1>
 
 <?php
-$file = 'files/' . $_POST['selected_file'];;
-echo $file;
-$content = json_decode(file_get_contents($file));
+$file = 'files/' . $_POST['selected_file'];
+
+//Enable Error Reporting and Display:
+/**
+error_reporting(~0);
+ini_set('display_errors', 1);
+
+if (ini_get('allow_url_fopen') == 1) {
+    echo '<p style="color: #0A0;">fopen is allowed on this host.</p>';
+} else {
+    echo '<p style="color: #A00;">fopen is not allowed on this host.</p>';
+}
+**/
+
+$content = file_get_contents($file);
+$content = json_decode($content);
+
+// Check for json errors
+var_dump(json_last_error());
+
 $questions = $content->questions;
 $advice = $content->advice;
 ?>
