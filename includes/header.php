f10412ee82a7fead9c5da87f2fa457e9d08a2845
katjam <katja@neontribe.co.uk>
2015-05-15 14:01:06 +0100

    detect base url as BASE

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,37 @@
 <?php session_start();
 // Globals
 define("BRAND", "SEAP");
-define("BASE", $_SERVER['REQUEST_URI']);
-print_r($_SERVER);
+define("BASE", base_url());
+
+function base_url($atRoot=FALSE, $atCore=FALSE, $parse=FALSE){
+  if (isset($_SERVER['HTTP_HOST'])) {
+    $http = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off' ? 'https' : 'http';
+    $hostname = $_SERVER['HTTP_HOST'];
+    $dir =  str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+    $core = preg_split('@/@', str_replace($_SERVER['DOCUMENT_ROOT'], '', realpath(dirname(__FILE__))), NULL, PREG_SPLIT_NO_EMPTY);
+    $core = $core[0];
+    $tmplt = $atRoot ? ($atCore ? "%s://%s/%s/" : "%s://%s/") : ($atCore ? "%s://%s/%s/" : "%s://%s%s");
+    $end = $atRoot ? ($atCore ? $core : $hostname) : ($atCore ? $core : $dir);
+    $base_url = sprintf( $tmplt, $http, $hostname, $end );
+  } else {
+  $base_url = 'http://localhost/';
+  }
+  if ($parse) {
+    $base_url = parse_url($base_url);
+    if (isset($base_url['path'])) if ($base_url['path'] == '/') $base_url['path'] = '';
+  }
+
+  return $base_url;
+}
+
 ?>
 <!DOCTYPE html>
 <html dir="ltr" lang="en-GB">
 <head>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<link href="<?=BASE ?>css/style.css" rel="stylesheet" media="all" />
-<link href="<?=BASE ?>css/seap-style.css" rel="stylesheet" media="all" />
+<link href="<?=BASE ?>/css/style.css" rel="stylesheet" media="all" />
+<link href="<?=BASE ?>/css/seap-style.css" rel="stylesheet" media="all" />
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,700,300" rel="stylesheet" type="text/css" media="none" onload="if(media!='all')media='all'">
 <title>JSON content editor - SEAP</title>
 </head>
@@ -22,10 +43,10 @@ print_r($_SERVER);
   <nav class="to-main-menu" role="navigation">
     <ul>
       <li>
-      <a href="<?=BASE ?>loadnewfile.php">File chooser</a>
+      <a href="<?=BASE ?>/loadnewfile.php">File chooser</a>
       </li>
       <li>
-      <a href="<?=BASE ?>help.php">Help</a>
+      <a href="<?=BASE ?>/help.php">Help</a>
       </li>
     </ul>
   </nav>
