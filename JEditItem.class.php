a48669a197568d9577dc2855d7863a0a56b4b950
katjam <katja@neontribe.co.uk>
2015-05-21 16:03:06 +0100

    Item handle function to class

---
+++ JEditItem.class.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ *  Item
+ */
+
+class JEditItem {
+
+  /**
+   * Trim and set empty string to null.
+   *
+   * @param string $value
+   *
+   * @return string or null $value
+   */
+  public static function cleanValue($value) {
+    // remove leading and trailing spaces
+    $value = trim($value);
+    // set empty string to null
+    if ($value === '') {
+      $value = null;
+    }
+    return $value;
+  }
+
+  /**
+   * Check for content in arrays/ objs
+   */
+  public static function hasValues($val) {
+    foreach ($val as $content) {
+      if ($content) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
