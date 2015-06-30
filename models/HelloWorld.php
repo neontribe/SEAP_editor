f9a886cf7ea0c6f2bf831795954e11535cba9804
katjam <katja@neontribe.co.uk>
2015-06-30 15:06:02 +0100

    Broken atoum tests SEAP-49

---
+++ models/HelloWorld.php
@@ -0,0 +1,9 @@
+namespace SEAP_editor\models;
+
+class HelloWorld
+{
+    public function getHiAtoum ()
+    {
+        return 'Hi atoum !';
+    }
+}
---
+++ tests/atoum/init.php
@@ -0,0 +1,20 @@
+require_once __DIR__ . '/../../models/HelloWorld.php';
+
+use atoum;
+
+/*
+ * Test class for Vendor\Project\HelloWorld
+ *
+ * Note that they had the same name that the tested class
+ * and that it derives frim the atoum class
+ */
+class HelloWorld extends atoum
+{
+    /*
+     * This method is dedicated to the getHiAtoum() method
+     */
+    public function testGetHiAtoum ()
+    {
+      $this->given($this->newTestedInstance)->string($this->testedInstance->getHiAtoum())->isEqualTo('Hello');       
+    }
+}
