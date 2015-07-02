ab0537ced4e864f9f5049e042ee85e5f19be9acc
katjam <katja@neontribe.co.uk>
2015-07-02 11:22:47 +0100

    Added php declaration to test class and test SEAP-49

--- models/HelloWorld.php
+++
@@ -1,9 +0,0 @@
-namespace SEAP_editor\models;
-
-class HelloWorld
-{
-    public function getHiAtoum ()
-    {
-        return 'Hi atoum !';
-    }
-}
--- tests/atoum/init.php
+++ tests/atoum/init.php
@@ -1,4 +1,5 @@
-require_once __DIR__ . '/../../models/HelloWorld.php';
+<?php 
+require_once '../../models/HelloWorld.class.php';
 
 use atoum;
 
