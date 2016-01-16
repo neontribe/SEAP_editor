1833963f278737c98314f8c74fc0a990477686a5
katjam <katja@neontribe.co.uk>
2016-01-16 17:27:58 +0000

    add test

---
+++ tests/units/JEditItem.php
@@ -0,0 +1,31 @@
+<?php
+namespace SEAP_editor\tests\units;
+require_once __DIR__ . '/../../JEditItem.class.php';
+use atoum;
+/*
+ * Test class for SEAP_editor\JEditItem
+ */
+class JEditItem extends atoum
+{
+    /*
+     * This method is dedicated to the cleanValue() method
+     */
+    public function testCleanValue()
+    {
+      $this
+        ->given($this->newTestedInstance)
+        ->string($this->testedInstance->cleanValue(' with lead and tail '))
+        ->isEqualTo('with lead and tail')
+      ;
+      $this
+        ->given($this->newTestedInstance)
+        ->string($this->testedInstance->cleanValue('with tail '))
+        ->isEqualTo('with tail')
+      ;
+      $this
+        ->given($this->newTestedInstance)
+        ->string($this->testedInstance->cleanValue('no lead nor tail'))
+        ->isEqualTo('no lead nor tail')
+      ;
+    }
+}
